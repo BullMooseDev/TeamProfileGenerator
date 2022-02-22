dc54308111d234148ca13227f028b55294ed8848
BullMooseDev <koleervine@gmail.com>
2022-02-22 09:42:22 -0700

    updated challanged w/ tutor

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/employee');
+/* const generateHtml = require('./lib/src/htmlTemplate'); */
+/* const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
-const Manager = require('./lib/manager');
+const Manager = require('./lib/manager'); */
 
-const questionsForProfileGen = () => {
+const determineEmployeeCount = () => {
     console.log('We are now creating a professional profile for your team!');
 
-    return inquirer
-        .prompt([{
+    return inquirer.prompt([
+        {
+            type: 'number',
+            message: 'how many employees do you have in total? (Required)',
+            name: 'employeeCount',
+            validate: employeeCount => {
+                if (employeeCount) {
+                    return true;
+                } else {
+                    console.log('you need to type how many members are on the team.');
+                    return false;
+                }
+            }
+        }
+    ]).then(function (numberResponse) {
+        console.log(numberResponse)
+        if (numberResponse.employeeCount < 2) {
+            return employeeDescription();
+        } else {
+            numberResponse.employeeCount.forEach(employeeCount => {
+                employeeDescription();
+            })
+        };
+    });
+};
+
+
+/* use a for loop to ask the employeedescription multiple times based on number prompt
+push each employee object into an array
+use for each to turn each object in array into a card onto html 
+
+also use foreach for each object in order to deconstruct and get individual data?*/
+
+const employeeDescription = () => {
+    return inquirer.prompt([
+        {
             type: 'input',
             message: "please enter the employee name. (Required)",
-            name: 'name',
+            name: 'employeeName',
             validate: nameInput => {
                 if (nameInput) {
                     return true;
@@ -25,7 +60,7 @@ const questionsForProfileGen = () => {
         {
             type: 'input',
             message: "please enter the employees email. (Required)",
-            name: 'role',
+            name: 'teamMemberEmail',
             validate: roleInput => {
                 if (roleInput) {
                     return true;
@@ -38,7 +73,7 @@ const questionsForProfileGen = () => {
         {
             type: 'input',
             message: "please input the employees github. (Required)",
-            name: 'github',
+            name: 'githubUserName',
             validate: roleInput => {
                 if (roleInput) {
                     return true;
@@ -49,24 +84,9 @@ const questionsForProfileGen = () => {
             }
         },
         {
-            type: 'list',
-            message: "please choose the employees role. (Required)",
-            name: 'role',
-            default: 'index',
-            choices: ['engineer', 'manager', 'intern'],
-            validate: roleInput => {
-                if (roleInput) {
-                    return true;
-                } else {
-                    console.log('you need to choose a role');
-                    return false;
-                }
-            }
-        },
-        {
             type: 'input',
             message: "please enter the employees id. (Required)",
-            name: 'id',
+            name: 'idNumber',
             validate: idInput => {
                 if (idInput) {
                     return true;
@@ -75,10 +95,82 @@ const questionsForProfileGen = () => {
                     return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            message: "please enter the employees office location. (Required)",
+            name: 'officeLocation',
+            validate: officeLocation => {
+                if (officeLocation) {
+                    return true;
+                } else {
+                    console.log('you need to type the office location');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            message: "please choose the employees role. (Required)",
+            name: 'role',
+            default: 'index',
+            choices: ['engineer', 'manager', 'intern']
         }
-        /* make it ask if theres more employees and if yes then run the inquirer again?
-        for every full employee profile data use bootstrap to create a card of their data */
-    ])
+    ]).then(function (responses) {
+        for (let i = 0; i < responses.role.length; i++) {
+            if (responses.role == "intern") {
+                return inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'attendedSchool',
+                        message: 'what school did the intern attend?',
+                        validate: schoolInput => {
+                            if (schoolInput) {
+                                return true;
+                            } else {
+                                console.log('you need to type a school');
+                                return false;
+                            }
+                        }
+                    }
+                ]);
+            } else {
+                return;
+            }
+        };
+    })
 };
 
-questionsForProfileGen();
+/* make it ask if theres more employees and if yes then run the inquirer again?
+use a for loop at beginning asking how many employees need to be made?
+
+function getAnswers() {
+return inquirer.prompt(questions).then((answers) => {
+    if (answers.is_finished) {
+    return answers;
+    } else {
+    return getAnswers();
+    }
+});
+}
+
+for every full employee profile data use bootstrap to create a card of their data */
+
+/* function writeToFile(readmeFile) {
+    fs.writeFile("./index.html", readmeFile, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Team profile created!')
+    });
+};
+
+questionsForProfileGen().then(answers => {
+    console.log(answers)
+    const newProfileTemplate = generateHtml(answers)
+    console.log(newProfileTemplate)
+    writeToFile(newProfileTemplate)
+}); */
+
+determineEmployeeCount();
